2fd332d4c89b0084b67458500b8f5b131067bb0c
i3amber <saoab1@hotmail.com>
2023-02-12 00:13:46 +0300

    update

---
+++ DeleviryGoo/JS/googleMapsAPI.js
@@ -0,0 +1,11 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 21.620289, lng: 39.174940 },
+    zoom: 14,
+  });
+}
+
+
+window.initMap = initMap;
