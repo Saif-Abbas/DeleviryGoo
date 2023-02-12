db7e5543856f469dc082d6cfc5888853a2fdc659
i3amber <saoab1@hotmail.com>
2023-02-12 03:22:59 +0300

    make a localhost

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use("/", express.static("./DeleviryGoo"));
+app.use("/", express.static(__dirname + "/DeleviryGoo"));
+app.get("/", (req, res) => { res.sendFile(__dirname + "/DeleviryGoo/home.html"); });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
